3940ca1b78c8a347b565bf1dd90cfe6ef783876b
Kimentii <malafeikirill@mail.ru>
2018-03-18 23:21:51 +0300

    Added fabric.

--- app/src/main/java/com/yatty/sevenatenine/client/MainActivity.java
+++ app/src/main/java/com/yatty/sevenatenine/client/MainActivity.java
@@ -12,9 +12,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.yatty.sevenatenine.api.in_commands.ErrorResponse;
 import com.yatty.sevenatenine.api.in_commands.LogInResponse;
 import com.yatty.sevenatenine.api.out_commands.LogInRequest;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -31,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         mConnectButton = findViewById(R.id.button_connect);
         mNameEditText = findViewById(R.id.et_name);
