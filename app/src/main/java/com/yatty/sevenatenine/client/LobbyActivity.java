df8f890bd496ebdd4580190f1124542bc4bb5f72
Kimentii <malafeikirill@mail.ru>
2018-05-12 10:20:27 +0300

    Fixed bug 1234.

--- app/src/main/java/com/yatty/sevenatenine/client/LobbyActivity.java
+++ app/src/main/java/com/yatty/sevenatenine/client/LobbyActivity.java
@@ -130,7 +130,7 @@ public class LobbyActivity extends AppCompatActivity {
             ((TextView) listView.findViewById(android.R.id.text1))
                     .setText(playerInfo.getPlayerId());
             ((TextView) listView.findViewById(android.R.id.text2))
-                    .setText(playerInfo.getRating());
+                    .setText(String.valueOf(playerInfo.getRating()));
             return listView;
         }
 
